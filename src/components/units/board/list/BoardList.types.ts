89ce2e6201829f18ac1668fc88c0dbde09779cb2
nwd0907 <nwd0907@newbizstart.com>
2023-01-26 09:53:16 +0900

    first commit

--- src/components/units/board/list/BoardList.types.ts
+++
@@ -1,8 +0,0 @@
-import type { MouseEvent } from "react";
-import type { IQuery } from "../../../../commons/types/generated/types";
-
-export interface IBoardListUIProps {
-  data?: Pick<IQuery, "fetchBoards">;
-  onClickMoveToBoardNew: () => void;
-  onClickMoveToBoardDetail: (event: MouseEvent<HTMLDivElement>) => void;
-}
--- src/components/units/boardComment/write/BoardCommentWrite.types.ts
+++
@@ -1,9 +0,0 @@
-import type { ChangeEvent } from "react";
-
-export interface IBoardCommentWriteUIProps {
-  onChangeWriter: (event: ChangeEvent<HTMLInputElement>) => void;
-  onChangePassword: (event: ChangeEvent<HTMLInputElement>) => void;
-  onChangeContents: (event: ChangeEvent<HTMLTextAreaElement>) => void;
-  onClickWrite: () => void;
-  contents: string;
-}
